513c92c290f52a508059b36d5c721c946e0999a0
kuanglei1986 <344960239@qq.com>
2020-12-29 09:36:36 +0800

    cookie读写

---
+++ Pro01/src/cn/xh/servlet/AServlet.java
@@ -0,0 +1,27 @@
+package cn.xh.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/AServlet")
+public class AServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //1.创建Cookie
+        Cookie c1 = new Cookie("name","zhangsan");
+        Cookie c2 = new Cookie("age","18");
+        //2.把cookie添加到response里
+        resp.addCookie(c1);
+        resp.addCookie(c2);
+    }
+}
