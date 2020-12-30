2822ff56e2c78e846906d5e788b9356178517144
kuanglei1986 <344960239@qq.com>
2020-12-30 09:59:19 +0800

    过滤器学习

--- Filter_subject/src/cn/xh/filter/FilterDemo01.java
+++ Filter_subject/src/cn/xh/filter/FilterDemo01.java
@@ -1,4 +1,27 @@
 package cn.xh.filter;
 
-public class FilterDemo01 {
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+//@WebFilter("/")
+//@WebFilter(urlPatterns = "/index",dispatcherTypes = DispatcherType.FORWARD)
+public class FilterDemo01 implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("init...");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("doFilter1");
+        filterChain.doFilter(servletRequest,servletResponse);
+        System.out.println("doFilter1回来了！");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy...");
+
+    }
+
 }
---
+++ Filter_subject/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 2020/5/25
+  Time: 14:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>hello</title>
+  </head>
+  <body>
+ <%System.out.println("index.jsp");%>
+  </body>
+</html>
