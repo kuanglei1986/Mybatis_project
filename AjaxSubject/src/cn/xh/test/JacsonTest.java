14e249095b74a38eff9d6b6b87d25e853919a0d5
kuanglei1986 <344960239@qq.com>
2020-12-31 17:49:40 +0800

    maven运行servlet

---
+++ AjaxSubject/src/cn/xh/test/JacsonTest.java
@@ -0,0 +1,15 @@
+package cn.xh.test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JacsonTest {
+    public void test01() throws IOException {
+
+    }
+}
